d963c22ccc5b7d009c5e771b11cac163f417553e
joju <whwndud92@gmail.com>
2017-05-14 03:47:54 +0900

    add menu bar and ui integration

--- Application/src/project/Main.java
+++ Application/src/project/Main.java
@@ -23,58 +23,23 @@ public class Main extends Application {
 
     ScrollBar scrollBar = new ScrollBar();
 
+    FXMLLoader loader;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        db.readFile();
-
-        checkBoxArray = new CheckBox[db.getAuthorSet().size()];
-
-        FXMLLoader loader = new FXMLLoader(
+        loader = new FXMLLoader(
                 getClass().getResource("main.fxml")
         );
         Pane root = loader.load();
-        Controller controller = loader.<Controller>getController();
-        controller.initManager(this);
 
         Scene scene = new Scene(root, 1024, 768);
-        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-
-
-        double height = scene.getHeight();
-
-        anchorPane.setMaxHeight(height);
-        scrollPane.setMinHeight(height-100);
-        scrollPane.setPrefHeight(height-100);
-        scrollPane.setMaxHeight(height);
-
-        int i=0;
-        for(Node author : db.getAuthorSet())
-        {
-            checkBoxArray[i] = new CheckBox();
-            checkBoxArray[i].setText(author.getName());
-            checkBoxArray[i].setLayoutX(10);
-            checkBoxArray[i].setLayoutY(10 + i * 20);
-            anchorPane.getChildren().add(i,checkBoxArray[i]);
-            i++;
-        }
-
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-        scrollPane.setContent(scrollBar);
-        scrollPane.setContent(anchorPane);
-        scrollPane.setLayoutX(14);
-        scrollPane.setLayoutY(14);
-        root.getChildren().addAll(scrollPane);
-        scrollBar.setLayoutX(scene.getWidth()-scrollBar.getWidth());
-        scrollBar.setMin(0);
-        scrollBar.setOrientation(Orientation.VERTICAL);
-        scrollBar.setPrefHeight(anchorPane.getHeight());
-
-
-        primaryStage.setTitle("Main Layout");
         primaryStage.setScene(scene);
-        primaryStage.show();
 
+        Controller controller = loader.<Controller>getController();
+        controller.initManager(this,root,primaryStage);
+
+        controller.transformToMainGraph();
     }
 
     public static void main(String[] args) {
