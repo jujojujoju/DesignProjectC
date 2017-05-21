9a2da5b1a829930e8368a1323fe4217b7407a1fb
joju <whwndud92@gmail.com>
2017-05-21 23:33:35 +0900

    second term last commit2

--- Application/src/sample/CircleCell.java
+++ Application/src/sample/CircleCell.java
@@ -18,6 +18,8 @@ public class CircleCell extends Cell {
         Circle view = new Circle(radius*10,Color.rgb(red>255?255:red,10,10));
         view.setStrokeWidth(3);
         Text text = new Text(id);
+        text.setStyle("-fx-border-color: aqua");
+        text.setFill(Color.YELLOWGREEN);
         StackPane stackPane = new StackPane();
         stackPane.getChildren().addAll(view,text);
 
